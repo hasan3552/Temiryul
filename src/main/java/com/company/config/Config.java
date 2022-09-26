5aec91bb9f0f61633cec53aa09ae4aa910954921
hasan <hasan6453552"gmail.com>
2022-09-26 13:10:06 +0500

    deploy for heroku

--- src/main/java/com/company/config/Config.java
+++ src/main/java/com/company/config/Config.java
@@ -15,9 +15,9 @@ public class Config {
 
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/train");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("hasan");
+        dataSource.setUrl("jdbc:postgresql://ec2-35-170-146-54.compute-1.amazonaws.com:5432/dej1c86jkjt528");
+        dataSource.setUsername("higselmbwgerqe");
+        dataSource.setPassword("de82a1cb482af42f77b41c8c868611600d2f64f57675cf674d08af1f7c05c921");
 
         JdbcTemplate jdbcTemplate = new JdbcTemplate();
         jdbcTemplate.setDataSource(dataSource);
